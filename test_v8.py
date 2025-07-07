9786bd70aceb411b720a8c88a1dc49cf33eb06cf
shiyaooo1110 <shiyaooo1110@gmail.com>
2025-07-07 16:11:07 +0800

    Add files via upload

---
+++ test_v8.py
@@ -0,0 +1,27 @@
+# # need to install the packages below
+# pip install ultralytics
+# pip install roboflow   
+# pip install ipython
+
+import ultralytics
+ultralytics.checks()
+
+from ultralytics import YOLO
+from IPython.display import Image, display
+
+# import dataset
+from roboflow import Roboflow
+rf = Roboflow(api_key="UBlf713RmivxrohpL8GX")
+project = rf.workspace("tunku-abdul-rahman-university-of-management-and-technology-ttt1n").project("wood-defects-8lcdv")
+version = project.version(7)
+dataset = version.download("yolov11")
+
+# # run in terminal to train model
+# yolo task=detect mode=train data=Wood-Defects-7/data.yaml model=yolov8n.pt epochs=8 imgsz=640
+# # results of YOLOv8 inside the train to train 6 folder
+
+# yolo task=detect mode=val model="runs\detect\train6\weights\best.pt" data=Wood-Defects-7/data.yaml
+# # Results saved to runs\detect\val
+
+# yolo task=detect mode=predict model="runs\detect\train6\weights\best.pt" conf=0.25 source=Wood-Defects-7\test\images save=True
+# # Results saved to runs\detect\predict
